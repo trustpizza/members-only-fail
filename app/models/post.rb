0b6a05489f402cd6c757dd46f98b3b11e81e9dea
trustpizza <maxelolsson@gmail.com>
2022-12-02 14:27:58 -0600

    Begin workaround for TurboController and Devise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
   belongs_to :user
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+class TurboController < ApplicationController
+
+  class Responder < ActionController::Responder
+    def to_turbo_stream
+      controller.render(optiions.merge(format: :html))
+    rescue ActionView::MissingTemplate => error 
+      if get?
+        raise error
+      elsif has_errors? &&
+      end
+    end
+  end
+
+  self.responder = Responder
+  respond_to :html, :turbo_stream
+end
+
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -18,7 +35,7 @@ Devise.setup do |config|
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
-  # config.parent_controller = 'DeviseController'
+  config.parent_controller = 'TurboController'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
